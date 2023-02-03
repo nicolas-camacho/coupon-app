f9f5f1d059094fade7fc118bdabbb5ccfcc2b99f
Nicolas Camacho <nicolas.aguilar1999@gmail.com>
2023-02-03 13:02:18 -0500

    :sparkles: Introducing download feature

--- src/components/Coupon/index.tsx
+++ src/components/Coupon/index.tsx
@@ -3,44 +3,45 @@ import Image from 'next/image'
 
 const Coupon = () => {
     return (
-        <div className={styles.coupon}>
-            <div className={styles.coupon_main}>
-                <div className={styles.main_header}>
-                    <h1>Doer</h1>
-                    <p>Nicolas Camacho</p>
-                </div>
-                <div className={styles.main_content}>
-                    <h2>Coupon for:</h2>
-                    <p>Free coffee</p>
-                </div>
-                <div className={styles.main_details}>
-                    <div className={styles.details_date}> 
-                        <h2>Valid until:</h2>
-                        <p>31/12/2021</p>
+        <div className={styles.container} id="coupon_container">
+            <div className={styles.coupon}>
+                <div className={styles.coupon_main}>
+                    <div className={styles.main_header}>
+                        <h1>Doer</h1>
+                        <p>Nicolas Camacho</p>
                     </div>
-                    <div className={styles.details_price}>
-                        <div>
-                            <h2>Price</h2>
-                            <p>$ 0.00</p>
+                    <div className={styles.main_content}>
+                        <h2>Coupon for:</h2>
+                        <p>Free coffee</p>
+                    </div>
+                    <div className={styles.main_details}>
+                        <div className={styles.details_date}> 
+                            <h2>Valid until:</h2>
+                            <p>31/12/2021</p>
+                        </div>
+                        <div className={styles.details_price}>
+                            <div>
+                                <h2>Price</h2>
+                                <p>$ 0.00</p>
+                            </div>
+                        </div>
+                        <div className={styles.details_location}>
+                            <p>
+                                <strong>Barranquilla - Atlantico / Colombia</strong>
+                            </p>
                         </div>
                     </div>
-                    <div className={styles.details_location}>
-                        <p>
-                            <strong>Barranquilla - Atlantico / Colombia</strong>
-                        </p>
+                </div>
+                <div className={styles.coupon_rip}></div>
+                <div className={styles.coupon_footer}>
+                    <div>
+                        <Image src="/qr-coupon-generator.svg" alt="Logo" width={100} height={100} />
                     </div>
-                </div>
-            </div>
-            <div className={styles.coupon_rip}></div>
-            <div className={styles.coupon_footer}>
-                <div>
-                    <Image src="/qr.svg" alt="Logo" width={100} height={100} />
-                </div>
-                <div className={styles.footer_logos}>
-                    <h2>Powered by</h2>
-                    <div className={styles.logos}>
-                        <Image src="/next.svg" alt="Logo" width={50} height={50} />
-                        <Image src="/vercel.svg" alt="Logo" width={50} height={50} />
+                    <div className={styles.footer_logos}>
+                        <h2>Powered by</h2>
+                        <div className={styles.logos}>
+                            <Image src="/vercel-logotype-dark.png" alt="Logo" width={100} height={25} />
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import html2canvas from 'html2canvas'
+//Styles
 import styles from '@/styles/Home.module.css'
 //Components
 import Coupon from '@/components/Coupon'
 
 
 export default function Home() {
+
+  const [editMode, setEditMode] = useState(false);
+  
+  const download = () => {
+    const coupon = document.getElementById('coupon_container')
+    if(!coupon) {
+      alert('No coupon found')
+    } else {
+      html2canvas(coupon).then(canvas => {
+        const link = document.createElement('a')
+        link.download = 'coupon.png'
+        link.href = canvas.toDataURL('image/png')
+        link.click()
+      })
+    }
+  }
+
   return (
     <>
       <Head>
@@ -14,6 +34,10 @@ export default function Home() {
         <link rel="icon" href="/coupon.png" />
       </Head>
       <main className={styles.main}>
+        <div className={styles.header}>
+          <button onClick={download}>Download</button>
+          <button>Edit</button>
+        </div>
         <Coupon />
       </main>
     </>
